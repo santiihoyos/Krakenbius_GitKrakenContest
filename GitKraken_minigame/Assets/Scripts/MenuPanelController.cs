10cec7f3209584f9eae19a18b7cf19085974971d
N2AGames <not2awesomegames@gmail.com>
2016-04-06 13:26:20 +0200

    Game Menu Design

---
+++ GitKraken_minigame/Assets/Scripts/MenuPanelController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuPanelController : MonoBehaviour {
+
+	// References Panel
+	public GameObject mainPanel;
+	public GameObject settingsPanel;
+	public GameObject creditsPanel;
+
+	//*********************************************** BUTTON RETURN MAIN MENU *****************************************************
+	public void HomeButton() 
+	{
+		((AudioSource) GameObject.Find("SoundButton").GetComponent<AudioSource>()).Play();
+		GameObject mainPanelGO = Instantiate (mainPanel) as GameObject;
+		Destroy (GameObject.Find ("SettingsPanel(Clone)"));
+		Destroy (GameObject.Find ("CreditsPanel(Clone)"));
+	}
+
+	//*********************************************** BUTTONS MAIN MENU *****************************************************
+	public void SettingsButton ()
+	{
+		((AudioSource) GameObject.Find("SoundButton").GetComponent<AudioSource>()).Play();
+		GameObject settingsPanelGO = Instantiate (settingsPanel) as GameObject;
+        Destroy (GameObject.Find ("MainPanel(Clone)"));
+	}
+
+	//*********************************************** OTHER BUTTONS *****************************************************
+
+	// IN SSETTINGSPANEL
+	public void CreditsButton () 
+	{
+		((AudioSource) GameObject.Find("SoundButton").GetComponent<AudioSource>()).Play();
+		GameObject creditsPanelGO = Instantiate (creditsPanel) as GameObject;
+        Destroy (GameObject.Find ("MainPanel(Clone)"));
+	}
+}
---
+++ GitKraken_minigame/Assets/Scripts/SettingsController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour {
+
+	public Toggle musicButton;
+	public Toggle soundButton;
+
+	private AudioSource soundMusicMenu;
+	private AudioSource soundButtonClick;
+	private AudioSource soundAmmoClick;
+	private AudioSource soundTapClick;
+
+	private int on = 1;
+	private int off = 0;
+
+	void Awake () {
+		soundMusicMenu = GameObject.Find ("MusicMenu").GetComponent<AudioSource>();
+		soundButtonClick = GameObject.Find ("SoundButton").GetComponent<AudioSource>();
+		soundTapClick = GameObject.Find ("SoundTap").GetComponent<AudioSource>();
+
+		MusicControl(PlayerPrefs.GetInt("music"));
+		SoundControl(PlayerPrefs.GetInt("sound"));
+	}
+
+
+	// **************************************************** SETTINGS CLICKS *************************************************************************
+	public void ClickMusicButton () {
+		if (GameObject.Find ("SoundButton") != null) {
+			((AudioSource) GameObject.Find("SoundButton").GetComponent<AudioSource>()).Play();
+		}
+
+		if (musicButton.isOn) {
+			print ("MUSIC ON");
+			PlayerPrefs.SetInt ("music", on);
+			MusicControl (on);
+		} else {
+			print ("MUSIC OFF");
+			PlayerPrefs.SetInt ("music", off);
+			MusicControl (off);
+		}
+	}
+
+	public void ClickSoundButton () {
+		if (GameObject.Find ("SoundButton") != null) {
+			((AudioSource) GameObject.Find("SoundButton").GetComponent<AudioSource>()).Play();
+		}
+
+		if (soundButton.isOn) {
+			print ("SOUND ON");
+			PlayerPrefs.SetInt ("sound", on);
+			SoundControl (on);
+		} else {
+			print ("SOUND OFF");
+			PlayerPrefs.SetInt ("sound", off);
+			SoundControl (off);
+		}
+	}
+
+
+	// **************************************************** SETTINGS CHANGES *************************************************************************
+	private void MusicControl (int value) {
+		if (value == 1) {
+			soundMusicMenu.mute = false;
+			musicButton.isOn = true;
+		} else {
+			soundMusicMenu.mute = true;
+			musicButton.isOn = false;
+		}
+	}
+
+	private void SoundControl (int value) {
+		if (value == 1) {
+			soundButtonClick.mute = false;
+			soundTapClick.mute = false;
+			soundButton.isOn = true;
+		} else {
+			soundButtonClick.mute = true;
+			soundTapClick.mute = true;
+			soundButton.isOn = false;
+		}
+	}
+}
