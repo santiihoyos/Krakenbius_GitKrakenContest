265fd251312cee7e36e91e0e49181e8d040f372c
SantiHoyos <santiago.hoyos93@gmail.com>
2016-04-06 17:48:59 +0200

    Ranking layout created v1
    I have created the first version of ranking layout.

---
+++ GitKraken_minigame/Assets/Scripts/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Player {
+
+	public string nick;
+	public int score;
+
+	public Player (string _nick, int _score)
+	{
+		this.nick = _nick;
+		this.score = _score;
+	}
+}
---
+++ GitKraken_minigame/Assets/Scripts/RankingScripts/CallerToServer.cs
@@ -0,0 +1,53 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System;
+using System.Text;
+using System.IO;
+
+public class CallerToServer : MonoBehaviour {
+
+
+	private string HttpRequestMaker(string jsonText, string serviceName){
+
+		print("Enviando peticion...");
+		WebRequest request = WebRequest.Create ("http://51.254.134.174/gitkraken/"+serviceName);
+		request.Method = "POST";
+
+
+		print("Preparando datos la petición...");
+		string postData = jsonText;
+		byte[] byteArray = Encoding.UTF8.GetBytes (postData);
+
+
+		print("Definiendo tipo de respuesta...");
+		((HttpWebRequest)request).Accept = "application/json";
+		request.ContentType = "application/json";
+		request.ContentLength = byteArray.Length;
+
+
+		print("Escribiendo en flujo de salida...");
+		Stream dataStream = request.GetRequestStream ();
+		dataStream.Write (byteArray, 0, byteArray.Length);
+		dataStream.Close ();
+
+
+		print("Obteniendo respuesta...");
+		WebResponse response = request.GetResponse ();
+		//print (((HttpWebResponse)response).StatusCode);
+		dataStream = response.GetResponseStream ();
+		StreamReader reader = new StreamReader (dataStream);
+		string responseFromServer = reader.ReadToEnd ();
+
+		//consola.text = responseFromServer;
+
+		print("Cerrando todo...");
+		reader.Close ();
+		dataStream.Close ();
+		response.Close ();
+
+		return responseFromServer;
+	}
+
+
+}
