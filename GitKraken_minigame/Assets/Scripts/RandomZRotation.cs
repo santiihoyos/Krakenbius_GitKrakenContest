d4e97d5002ea1594e52befb15bc095e62bcfa6cd
unknown <karlossius@gmail.com>
2016-04-08 09:18:09 +0200

    REBASE effect + animation Background

---
+++ GitKraken_minigame/Assets/Scripts/RandomZRotation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomZRotation : MonoBehaviour {
+
+    public float minRotationSpeed=20;
+    public float maxRotationSpeed = 50;
+
+
+    float rotationSpeed;
+    float timeRotating;
+    int rotationDirection = 1;
+    void Start()
+    {
+        rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+        timeRotating = Random.Range(0,20f);
+        StartCoroutine(ChangeRotation());
+    }
+	
+	void Update ()
+    {
+        transform.Rotate(0,0,rotationDirection*rotationSpeed*Time.deltaTime);
+	}
+
+    IEnumerator ChangeRotation()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeRotating);
+            rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+            timeRotating = Random.Range(0, 20f);
+            rotationDirection = -rotationDirection;
+        }
+
+    }
+}
