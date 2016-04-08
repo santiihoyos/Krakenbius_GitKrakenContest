6ddaf1e42b112153ab87d712bc5fe3c30e273d91
SantiHoyos <santiago.hoyos93@gmail.com>
2016-04-08 12:52:46 +0200

    Ranking 100% OK

--- GitKraken_minigame/Assets/Scripts/RankingScripts/RankingManager.cs
+++ GitKraken_minigame/Assets/Scripts/RankingScripts/RankingManager.cs
@@ -8,14 +8,20 @@ using UnityEngine.Experimental.Networking;
 public class RankingManager : MonoBehaviour {
 
 	public GameObject prefabItemRanking;
+	private int pos;
 
 	void Start(){
 
 		StartCoroutine (LoadRanking());
+
+		StartCoroutine (CheckInRankingReq(300));
+
+		//StartCoroutine (UpdateRanking("Amo Santi",300));
 	}
 		
 
 	IEnumerator LoadRanking() {
+
 		UnityWebRequest www = UnityWebRequest.Get("http://51.254.134.174/webservices/gitkraken/ranking/LoadRanking.php");
 		yield return www.Send();
 
@@ -31,26 +37,74 @@ public class RankingManager : MonoBehaviour {
 
 		SimpleJSON.JSONNode nodo_principal= SimpleJSON.JSON.Parse (respuesta_json);
 
+
 		IEnumerator enumerator= nodo_principal.Childs.GetEnumerator ();
 		enumerator.MoveNext ();
 
 		Transform contenedorItems = GameObject.Find ("PlayersContainer").transform;
 
-		int pos = 1;
+		int pos = 0;
 
 		foreach (SimpleJSON.JSONClass player in ((SimpleJSON.JSONNode)enumerator.Current).AsArray) {
 
-			Player currentPlayer=JsonUtility.FromJson(player.ToString(),typeof(Player)) as Player;
+			Player tmp = JsonUtility.FromJson(player.ToString(),typeof(Player)) as Player;
 
+			if(prefabItemRanking!=null){
 			GameObject nuevoItem = Instantiate (prefabItemRanking);
-
 			nuevoItem.transform.SetParent (contenedorItems, false);
-
-			nuevoItem.transform.GetChild (0).gameObject.GetComponent<Text> ().text = pos.ToString();
-			nuevoItem.transform.GetChild (1).gameObject.GetComponent<Text>().text = currentPlayer.nick.ToUpper();
-			nuevoItem.transform.GetChild (2).gameObject.GetComponent<Text> ().text = currentPlayer.score.ToString ();
+				nuevoItem.transform.GetChild (0).gameObject.GetComponent<Text> ().text = (pos+1).ToString();
+				nuevoItem.transform.GetChild (1).gameObject.GetComponent<Text>().text = tmp.nick.ToUpper();
+				nuevoItem.transform.GetChild (2).gameObject.GetComponent<Text> ().text = tmp.score.ToString ();
+			}
 
 			pos++;
 		}
 	}
+		
+
+	IEnumerator CheckInRankingReq(int score) {
+
+		WWWForm formulario = new WWWForm ();
+		formulario.AddField ("score_player", score);
+
+		UnityWebRequest www = UnityWebRequest.Post("http://51.254.134.174/webservices/gitkraken/ranking/CheckInRanking.php",formulario);
+
+		yield return www.Send();
+
+		if(www.isError) {
+			print ("Ojo con la implementación CORS de seguridad de JavaScript");
+		}
+		else {
+			
+
+			DrawDialog (System.Convert.ToInt32(www.downloadHandler.text));
+		}
+	}
+
+
+	public void DrawDialog(int posEnRanking){
+	
+		print ("POSICION con ese escore= "+posEnRanking);
+
+	}
+
+
+	IEnumerator UpdateRanking(string nick, int score) {
+
+		WWWForm formulario = new WWWForm ();
+		formulario.AddField ("nick_player", nick);
+		formulario.AddField ("score_player", score);
+
+		UnityWebRequest www = UnityWebRequest.Post("http://51.254.134.174/webservices/gitkraken/ranking/UpdateRanking.php",formulario);
+
+		yield return www.Send();
+
+		if(www.isError) {
+			print ("Ojo con la implementación CORS de seguridad de JavaScript");
+		}
+		else {
+			print ("Resultado de la insercion= " + www.downloadHandler.text);
+		}
+	}
+
 }
