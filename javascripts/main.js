24954366fc2de489294a8dae7e4ee3c1664c2f2e
AngelCid <angel.cid@gmail.com>
2016-04-11 08:51:46 +0200

    ajustando el tamaño, quitados botones de google+ y facebook.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,5 @@
-console.log('This would be the main JS file.');
 // Twitter
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-
-// Facebook
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 // Module for GKGAME
 var Module = {
