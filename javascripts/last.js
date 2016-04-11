3c7dc0822531ed68652f7ffeb6533c7ebea2d86a
AngelCid <angel.cid@gmail.com>
2016-04-11 21:52:09 +0200

    el E no existe porque lo he sacado de la funcion. su valor es 1

--- javascripts/last.js
+++ javascripts/last.js
@@ -3,7 +3,7 @@ full.onclick = function() { console.log("maximize");
     var t = JSEvents.canPerformEventHandlerRequests;
     JSEvents.canPerformEventHandlerRequests = function() {
         return 1
-    }, Module.cwrap("SetFullscreen", "void", ["number"])(e), JSEvents.canPerformEventHandlerRequests = t; 
+    }, Module.cwrap("SetFullscreen", "void", ["number"])(1), JSEvents.canPerformEventHandlerRequests = t; 
     console.log("end maximize");
 };
 
