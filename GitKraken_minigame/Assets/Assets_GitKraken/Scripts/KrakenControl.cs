0c2e0ae5f3f6a46c094cb610ee97650898c55803
JesusRodriguezDeveloper <jesusrodriguezdeveloper@gmail.com>
2016-04-11 10:06:12 +0200

    bubble finished

--- GitKraken_minigame/Assets/Assets_GitKraken/Scripts/KrakenControl.cs
+++ GitKraken_minigame/Assets/Assets_GitKraken/Scripts/KrakenControl.cs
@@ -4,18 +4,28 @@ using System.Collections;
 public class KrakenControl : MonoBehaviour {
 
 	public static int score;
+	GameObject kraken;
 
 	// Use this for initialization
 	void Start () {
+		kraken = GameObject.Find ("Kraken");
 		score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.A)) {
-			this.gameObject.transform.Rotate (Vector3.forward * 200f * Time.deltaTime);
-		} else if (Input.GetKey(KeyCode.D)) {
-			this.gameObject.transform.Rotate (Vector3.back * 200f * Time.deltaTime);
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			RotateLeft ();
+		} else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+			RotateRight ();
 		}
+	}
+
+	public void RotateLeft() {		
+		kraken.gameObject.transform.Rotate (Vector3.forward * 200f * Time.deltaTime);
+	}
+
+	public void RotateRight() {		
+		kraken.gameObject.transform.Rotate (Vector3.back * 200f * Time.deltaTime);
 	}
 }
