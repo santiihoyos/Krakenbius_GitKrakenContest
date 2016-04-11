7bea4f126f80b373cad354c9d9c34e8b8016f3d5
AngelCid <angel.cid@gmail.com>
2016-04-11 21:48:41 +0200

    sobreescribo el javascript del boton.

--- javascripts/last.js
+++ javascripts/last.js
@@ -1,3 +1,9 @@
 var full = document.getElementById("FullScreen");
-full.onclick = function() { console.log("maximize");SetFullscreen(1); console.log("end maximize");};
+full.onclick = function() { console.log("maximize");
+    var t = JSEvents.canPerformEventHandlerRequests;
+    JSEvents.canPerformEventHandlerRequests = function() {
+        return 1
+    }, Module.cwrap("SetFullscreen", "void", ["number"])(e), JSEvents.canPerformEventHandlerRequests = t; 
+    console.log("end maximize");
+};
 
